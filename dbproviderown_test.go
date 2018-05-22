ae61ca49e789fd529e1c3742b442a546252eaa52
Ragnar-BY <roman.buzuk@gmail.com>
2018-05-22 17:02:27 +0300

    refactoring

--- dbproviderown_test.go
+++ dbproviderown_test.go
@@ -21,37 +21,38 @@ func (db *dbmock) Write(key string, value string) error {
 	}
 	return nil
 }
+
 func TestDBProvider_ReadValue(t *testing.T) {
 	db := &dbmock{}
 	dp := DBProvider{db}
 
 	tt := []struct {
-		name     string
-		key      string
-		expected string
-		err      error
+		testName      string
+		key           string
+		expectedValue string
+		expectedError error
 	}{
 		{
-			name:     "right key",
-			key:      "rightkey",
-			expected: "value",
-			err:      nil,
+			testName:      "right key",
+			key:           "rightkey",
+			expectedValue: "value",
+			expectedError: nil,
 		},
 		{
-			name:     "wrong key",
-			key:      wrongkey,
-			expected: "",
-			err:      ErrWrongKey,
+			testName:      "wrong key",
+			key:           wrongkey,
+			expectedValue: "",
+			expectedError: ErrWrongKey,
 		},
 	}
 
 	for _, tc := range tt {
 		val, err := dp.ReadValue(tc.key)
-		if err != tc.err {
-			t.Errorf("[%s] expected %v, received %v", tc.name, tc.err, err)
+		if err != tc.expectedError {
+			t.Errorf("[%s] expected %v, received %v", tc.testName, tc.expectedError, err)
 		}
-		if val != tc.expected {
-			t.Errorf("[%s] expected %v, received %v", tc.name, tc.expected, val)
+		if val != tc.expectedValue {
+			t.Errorf("[%s] expected %v, received %v", tc.testName, tc.expectedValue, val)
 		}
 	}
 }
@@ -61,26 +62,26 @@ func TestDBProvider_AddValue(t *testing.T) {
 	dp := DBProvider{db}
 
 	tt := []struct {
-		name string
-		key  string
-		err  error
+		testName      string
+		key           string
+		expectedError error
 	}{
 		{
-			name: "right key",
-			key:  "rightkey",
-			err:  nil,
+			testName:      "right key",
+			key:           "rightkey",
+			expectedError: nil,
 		},
 		{
-			name: "wrong key",
-			key:  wrongkey,
-			err:  ErrWrongKey,
+			testName:      "wrong key",
+			key:           wrongkey,
+			expectedError: ErrWrongKey,
 		},
 	}
 
 	for _, tc := range tt {
 		err := dp.AddValue(tc.key, "val")
-		if err != tc.err {
-			t.Errorf("[%s] expected %v, received %v", tc.name, tc.err, err)
+		if err != tc.expectedError {
+			t.Errorf("[%s] expected %v, received %v", tc.testName, tc.expectedError, err)
 		}
 	}
 }
